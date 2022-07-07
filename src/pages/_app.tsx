9e67faa48be511e44d02e51bacafdc50d2dbb326
Leticia Taborda <leticia.s.taborda@gmail.com>
2022-07-07 18:22:46 -0300

    Add nextAuth API to authenticate by github

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,15 +1,16 @@
 import { AppProps } from 'next/app';
+import { SessionProvider } from 'next-auth/react';
 
 import '../styles/global.scss';
 
 import { Header } from '../components/Header';
 
-function MyApp({ Component, pageProps }: AppProps) {
+function MyApp({ Component, pageProps: { session, ...pageProps } }: AppProps) {
   return (
-    <>
+    <SessionProvider session={session}>
       <Header />
       <Component {...pageProps} />
-    </>
+    </SessionProvider>
   );
 }
 
