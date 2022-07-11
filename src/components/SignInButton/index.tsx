5db482e7167885328a3cd93d7a38e5f344cf9550
Leticia Taborda <leticia.s.taborda@gmail.com>
2022-07-11 17:53:45 -0300

    Config webhooks to listen Stripe events

--- src/components/SignInButton/index.tsx
+++ src/components/SignInButton/index.tsx
@@ -7,8 +7,6 @@ import styles from './styles.module.scss';
 export function SignInButton() {
   const { data: session, status } = useSession();
 
-  console.log(session, status);
-
   const isUserAuthenticated = status === 'authenticated';
 
   return isUserAuthenticated ? (
